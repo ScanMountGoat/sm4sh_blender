2186a0b9d24fb615a552748c8321b93beaaf8353
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2025-08-05 13:32:25 -0500

    add basic material and texture import

--- sm4sh_blender/import_model.py
+++ sm4sh_blender/import_model.py
@@ -6,7 +6,7 @@ import math
 
 from . import sm4sh_model_py
 
-from mathutils import Matrix, Quaternion
+from mathutils import Matrix
 
 
 def init_logging():
@@ -20,6 +20,12 @@ def import_nud_model(
     context,
     model: sm4sh_model_py.NudModel,
 ) -> Optional[bpy.types.Object]:
+    # These images will be referenced later by name.
+    # TODO: Check for duplicates?
+    pngs = sm4sh_model_py.decode_images_png(model.textures)
+    for texture, png in zip(model.textures, pngs):
+        import_image(texture, png)
+
     armature = None
     bone_names = []
     if model.skeleton is not None:
@@ -86,6 +92,15 @@ def import_mesh(
     # Convert from Y up to Z up.
     y_up_to_z_up = Matrix.Rotation(math.radians(90), 4, "X")
     blender_mesh.transform(y_up_to_z_up)
+
+    if material := mesh.material1:
+        blender_mesh.materials.append(import_material(material))
+    if material := mesh.material2:
+        blender_mesh.materials.append(import_material(material))
+    if material := mesh.material3:
+        blender_mesh.materials.append(import_material(material))
+    if material := mesh.material4:
+        blender_mesh.materials.append(import_material(material))
 
     obj = bpy.data.objects.new(blender_mesh.name, blender_mesh)
 
@@ -201,3 +216,80 @@ def import_weight_groups(
             # TODO: Is there a faster way than setting weights per vertex?
             for weight in influence.weights:
                 group.add([weight.vertex_index], weight.weight, "REPLACE")
+
+
+def import_material(material: sm4sh_model_py.NudMaterial) -> bpy.types.Material:
+    name = f"{material.flags:08X}"
+    blender_material = bpy.data.materials.new(name)
+
+    # Use custom properties to preserve values that are hard to represent in Blender.
+    blender_material["src_factor"] = str(material.src_factor).lstrip("SrcFactor.")
+    blender_material["dst_factor"] = str(material.dst_factor).lstrip("DstFactor.")
+    blender_material["alpha_func"] = str(material.alpha_func).lstrip("AlphaFunc.")
+    blender_material["cull_mode"] = str(material.cull_mode).lstrip("CullMode.")
+
+    for prop in material.properties:
+        if prop.name == "NU_materialHash":
+            material_hash = float32_bits(prop.values[0])
+            blender_material["NU_materialHash"] = f"{material_hash:08X}"
+
+    blender_material.use_nodes = True
+    nodes = blender_material.node_tree.nodes
+    links = blender_material.node_tree.links
+
+    # Create the nodes from scratch to ensure the required nodes are present.
+    # This avoids hard coding names like "Material Output" that depend on the UI language.
+    nodes.clear()
+
+    bsdf = nodes.new("ShaderNodeBsdfPrincipled")
+
+    output_node = nodes.new("ShaderNodeOutputMaterial")
+
+    links.new(bsdf.outputs["BSDF"], output_node.inputs["Surface"])
+
+    # TODO: Does preserving the order matter?
+    for prop in material.properties:
+        if prop.name != "NU_materialHash":
+            # TODO: Custom node for xyzw values?
+            node = nodes.new("ShaderNodeRGB")
+            node.label = prop.name
+            node.outputs[0].default_value = prop.values[:4]
+
+    for i, texture in enumerate(material.textures):
+        # TODO: Create images ahead of time?
+        node = nodes.new("ShaderNodeTexImage")
+        node.label = str(i)
+
+        # TODO: Load global textures like color ramps.
+        image_name = f"{texture.hash:08X}"
+        image = bpy.data.images.get(image_name)
+        if image is None:
+            image = bpy.data.images.new(image_name, 4, 4, alpha=True)
+
+        node.image = image
+    return blender_material
+
+
+import struct
+
+
+def float32_bits(f: float) -> int:
+    return struct.unpack("@I", struct.pack("@f", f))[0]
+
+
+def import_image(image: sm4sh_model_py.ImageTexture, png: bytes):
+    name = f"{image.hash_id:08X}"
+
+    blender_image = bpy.data.images.new(name, image.width, image.height, alpha=True)
+
+    # Loading png bytes is faster than foreach_set with a float buffer.
+    blender_image.pack(data=png, data_len=len(png))
+    blender_image.source = "FILE"
+
+    # TODO: This should depend on srgb vs linear in format.
+    blender_image.colorspace_settings.is_data = True
+
+    # Necessary for 0 alpha to not set RGB to black.
+    blender_image.alpha_mode = "CHANNEL_PACKED"
+
+    return blender_image
